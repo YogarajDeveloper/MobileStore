bfe05e984574a64a7a49d008a27e9f3b4eadd3d7
Yogaraj S <yogaraj@cloudrevelinnovation.com>
2026-07-24 11:25:28 +0400

    custom hooke and login flow added

--- frontend/src/Pages/Login/Login.jsx
+++ frontend/src/Pages/Login/Login.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import axios from "axios";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -60,66 +59,3 @@ const Login = () => {
 };
 
 export default Login;
-=======
-import axios from "axios";
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-
-const Login = () => {
-
-  const navigate = useNavigate();
-  
-  const [formdata, setFormdata] = useState({
-    email: "",
-    password: "",
-  });
-
-  const handleOnchange = (data) => {
-    setFormdata((prev) => ({
-      ...prev,
-      [data.target.name]: data?.target?.value,
-    }));
-  };
-
-  const handleSubmit = async (e) => {
-    try {
-      e.preventDefault();
-      const response = await axios.post(
-        "http://localhost:8080/api/auth/login",
-        formdata,
-        navigate("/dashboard")
-      );
-    } catch (error) {
-      alert(error);
-    }
-  };
-  return (
-    <form onSubmit={handleSubmit}>
-      <br />
-      <label htmlFor="email">Email:</label>
-      <input
-        name="email"
-        value={formdata?.email}
-        onChange={(e) => handleOnchange(e)}
-        type="email"
-        id="email"
-      />
-      <br />
-      <br />
-      <label htmlFor="password">Password:</label>
-      <input
-        name="password"
-        value={formdata?.password}
-        onChange={(e) => handleOnchange(e)}
-        type="password"
-        id="password"
-      />
-      <br />
-      <br />
-      <button type="submit">LOGIN</button>
-    </form>
-  );
-};
-
-export default Login;
->>>>>>> 0874c1d (custom hooke and login flow added)
--- frontend/src/Pages/Login/Register.jsx
+++ frontend/src/Pages/Login/Register.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import axios from "axios";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -58,64 +57,3 @@ const Register = () => {
 };
 
 export default Register;
-=======
-import axios from "axios";
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-
-const Register = () => {
-
-  const navigate = useNavigate();
-
-  const [formdata, setFormdata] = useState({
-    name: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
-    phone: "",
-  });
-  const handleSubmit = async (e) => {
-    try {
-      e.preventDefault();
-      const response = await axios.post(
-        "http://localhost:8080/api/users/store",
-        formdata,
-      );
-      navigate("/login")
-
-    } catch (error) {
-      alert(error);
-    }
-  };
-
-  const handleOnchange=(data)=>{
-    setFormdata((prev)=>({
-      ...prev,
-      [data.target.name ]:data?.target?.value  
-    }))
-  }
-
-  return (
-    <form onSubmit={handleSubmit}><br />
-      <label htmlFor="name">Username:</label>
-      <input name="name" value={formdata?.name}  onChange={(e)=>handleOnchange(e)} type="text"  /> <br /><br />
-
-      <label htmlFor="email">Email:</label>
-      <input name="email" value={formdata?.email} onChange={(e)=>handleOnchange(e)} type="email" id="email" /> <br /><br />
-
-      <label htmlFor="password">Password:</label>
-      <input name="password" value={formdata?.password} onChange={(e)=>handleOnchange(e)} type="password" id="password" /> <br /><br />
-
-      <label htmlFor="confirmPassword">Confirm Password:</label>
-      <input name="confirmPassword" value={formdata?.confirmPassword} onChange={(e)=>handleOnchange(e)} type="password" id="confirmPassword" /><br /><br />
-
-      <label htmlFor="phone">Phone:</label>
-      <input name="phone" value={formdata?.phone} onChange={(e)=>handleOnchange(e)} type="tel" id="phone" /><br /><br />
-
-      <button type="submit">Submit</button>
-    </form>
-  );
-};
-
-export default Register;
->>>>>>> 0874c1d (custom hooke and login flow added)

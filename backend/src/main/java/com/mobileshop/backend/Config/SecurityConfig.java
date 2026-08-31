66f02caaf9bedc912fac2bda34bb61e8d4ca16f9
Yogaraj S <yogaraj@cloudrevelinnovation.com>
2026-08-31 13:16:09 +0400

    Auth Coreection

--- backend/src/main/java/com/mobileshop/backend/Config/SecurityConfig.java
+++ backend/src/main/java/com/mobileshop/backend/Config/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig {
                 .requestMatchers(org.springframework.http.HttpMethod.OPTIONS, "/**").permitAll()
 
                 // Public APIs
-                .requestMatchers("/", "/error", "/api/users/store", "/api/auth/login", "/api/auth/google-login", "/api/auth/register").permitAll()
+                .requestMatchers("/", "/error", "/api/users/register", "/api/auth/login", "/api/auth/google-login").permitAll()
 
                 // Everything else requires JWT
                 .anyRequest().authenticated()
